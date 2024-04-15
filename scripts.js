37aaf27a09e9c39985b0ec89f04509bc431c9192
Yusif Mehdiyev <yusifmehdiyev55@gmail.com>
2024-04-15 19:40:13 +0400

    fixed button links

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// highlight current button
+const current = 0;
+for (var i = 0; i < document.links.length; i++) {
+    if (document.links[i].href === document.URL) {
+        current = i;
+    }
+}
+document.links[current].className = 'current';
